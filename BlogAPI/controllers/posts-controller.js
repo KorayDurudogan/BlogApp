5d4fc9650c48426cc07f50d7cca94e68ff956162
Koray Durudogan <koray.durudogan@outlook.com>
2020-05-03 15:44:23 +0300

    post fetch function made async

--- BlogAPI/controllers/posts-controller.js
+++ BlogAPI/controllers/posts-controller.js
@@ -25,32 +25,49 @@ router.put('/', function (req, res) {
     }
 });
 
+//get posts of followed people. Returns a promise.
+function fetchFollowedPosts(req, following) {
+    return new Promise((resolve, reject) => {
+        mongoDb.getDb().collection(Constants.users_collection).find({ _id: { $in: following } })
+            .toArray((err, result) => {
+                if (err) {
+                    MakeLogging(new Log(req.userData._uid, err));
+                    reject(res.status(404).send({ message: 'Fetching feed failed !' }));
+                }
+                else {
+                    let followedPosts = new PostIterator([]);
+                    result.forEach((user) => {
+                        //if user has any posts, filter publics and add to the post iterator.
+                        if (user.posts) {
+                            let filtered_posts = new PostIterator(user.posts.filter(e => !e.is_private));
+                            followedPosts.add(filtered_posts.get_posts());
+                        }
+                    });
+                    resolve(followedPosts);
+                }
+            });
+    });
+}
+
 //see posts of followed users.
 router.get('/', function (req, res) {
-    const user_collection = mongoDb.getDb().collection(Constants.users_collection);
-    user_collection.findOne({ _id: ObjectId(req.userData._uid) })
-        .then((result) => {
+    mongoDb.getDb().collection(Constants.users_collection).findOne({ _id: ObjectId(req.userData._uid) })
+        .then(async (result) => {
             try {
                 var logedin_user_posts = new PostIterator(result.posts);
 
                 //if user following other users, we are getting their posts too.
                 if (result.following) {
                     let following = result.following.map(id => ObjectId(id));
-                    user_collection.find({ _id: { $in: following } })
-                        .toArray((err, result) => {
-                            if (err) {
-                                MakeLogging(new Log(req.userData._uid, err));
-                                return res.status(404).send({ message: 'Fetching feed failed !' });
-                            }
-                            else {
-                                result.forEach((user) => {
-                                    //if user has any posts, filter publics and add to the post iterator.
-                                    if (user.posts) {
-                                        const filtered_posts = user.posts.filter(e => !e.is_private);
-                                        logedin_user_posts.add(filtered_posts);
-                                    }
-                                });
-                            }
+
+                    await fetchFollowedPosts(req, following)
+                        .then((followed_posts) => {
+                            if (followed_posts)
+                                logedin_user_posts.add(followed_posts.get_posts());
+                        })
+                        .catch((err) => {
+                            MakeLogging(new Log(req.userData._uid, err));
+                            return res.status(404).send({ message: 'Something went wrong ! Please try again later.' });
                         });
                 }
 
