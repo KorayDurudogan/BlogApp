eac5d2e630259008eb2baf75961662a94256874d
Koray Durudogan <koray.durudogan@outlook.com>
2020-05-03 23:15:33 +0300

    fetching users made async

--- BlogAPI/controllers/users-controller.js
+++ BlogAPI/controllers/users-controller.js
@@ -43,15 +43,87 @@ router.put('/unfollow', function (req, res) {
     }
 });
 
+function fetchFollowedUsers(req, following, res) {
+    return new Promise((resolve, reject) => {
+        mongoDb.getDb().collection(Constants.users_collection).find({ _id: { $in: following } })
+            .toArray((err, result) => {
+                if (err) {
+                    MakeLogging(new Log(req.userData._uid, err));
+                    reject(res.status(404).send({ message: 'Fetching feed failed !' }));
+                }
+                else {
+                    result = result.map(function (u) {
+                        return { _id: u._id, email: u.email, is_followed: true };
+                    });
+                    resolve(result);
+                }
+            });
+    });
+}
+
+function fetchUnfollowedUsers(req, following, res) {
+    return new Promise((resolve, reject) => {
+        mongoDb.getDb().collection(Constants.users_collection).find({ $and: [{ _id: { $nin: following } }, { _id: { $ne: ObjectId(req.userData._uid) } }] })
+            .toArray((err, result) => {
+                if (err) {
+                    MakeLogging(new Log(req.userData._uid, err));
+                    reject(res.status(404).send({ message: 'Fetching feed failed !' }));
+                }
+                else {
+                    result = result.map(function (u) {
+                        return { _id: u._id, email: u.email, is_followed: false };
+                    });
+                    resolve(result);
+                }
+            });
+    });
+}
+
+function fetchEveryoneExceptLogedIn(req, res) {
+    return new Promise((resolve, reject) => {
+        mongoDb.getDb().collection(Constants.users_collection).find({ _id: { $ne: ObjectId(req.userData._uid) } })
+            .toArray((err, result) => {
+                if (err) {
+                    MakeLogging(new Log(req.userData._uid, err));
+                    reject(res.status(404).send({ message: 'Fetching feed failed !' }));
+                }
+                else {
+                    result = result.map(function (u) {
+                        return { _id: u._id, email: u.email, is_followed: false };
+                    });
+                    resolve(result);
+                }
+            });
+    });
+}
+
 //fetchs all users except the loged in one.
 router.get('/', function (req, res) {
-    mongoDb.getDb().collection(Constants.users_collection).find({ _id: { $ne: ObjectId(req.userData._uid) } })
-        .toArray((err, result) => {
-            if (err) {
-                MakeLogging(new Log(req.userData._uid, err));
-                return res.status(404).send({ message: 'User fetching failed !' });
+    mongoDb.getDb().collection(Constants.users_collection).findOne({ _id: { $eq: ObjectId(req.userData._uid) } })
+        .then(async (result) => {
+            let following = result.following;
+
+            //if user has followings.
+            if (following) {
+                following = following.map(f => ObjectId(f));
+
+                const [followed_users, unfollowed_users] = await Promise.all([
+                    fetchFollowedUsers(req, following, res),
+                    fetchUnfollowedUsers(req, following, res)
+                ]);
+
+                const sonuc = followed_users.concat(unfollowed_users);
+                return res.status(200).send({ users: sonuc });
             }
-            return res.status(200).send({ users: result });
+            //if user doesnt follow anyone.
+            else {
+                const all_users_excep_logedin = await fetchEveryoneExceptLogedIn(req, res);
+                return res.status(200).send({ users: all_users_excep_logedin });
+            }
+        })
+        .catch((error) => {
+            MakeLogging(new Log(req.userData._uid, error));
+            return res.status(400).send({ message: 'Following transaction failed !' });
         });
 });
 
